1330254c71a937c4d50ffec8259d7f021d65fe8f
Eloy <vporcar93@gmail.com>
2023-04-07 13:08:45 +0200

    calm

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -2,7 +2,7 @@ import * as Joi from 'joi';
 
 
 export const JoiValidationSchema = Joi.object({
-  MONGODB: Joi.required(),
+  MONGODB_DATABASE: Joi.required(),
   PORT: Joi.number().default(3005),
   DEFAULT_LIMIT: Joi.number().default(6)
 })
